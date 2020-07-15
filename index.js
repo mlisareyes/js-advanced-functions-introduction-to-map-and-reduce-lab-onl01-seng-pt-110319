fbac741dd34d259620d237b2438e7d2a03c05cfe
mlisareyes <mlisareyes@gmail.com>
2020-07-14 21:07:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function mapToNegativize(numArray) {
+  return numArray.map(num => num * -1)
+}
+
+function mapToNoChange(numArray) {
+  return numArray
+}
+
+function mapToDouble(numArray) {
+  return numArray.map(num => num * 2)
+}
+
+function mapToSquare(numArray) {
+  return numArray.map(num => num ** 2)
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+  let total = startingPoint
+
+  sourceArray.forEach(function(source) {
+    total += source;
+  });
+  return total
+}
+
+function reduceToAllTrue(sourceArray) {
+  return sourceArray.every(Boolean)
+}
+
+function reduceToAnyTrue(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    if (sourceArray[i])
+      return true
+  };
+  return false;
+};
